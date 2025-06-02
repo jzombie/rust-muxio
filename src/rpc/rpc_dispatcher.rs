2ac5f29616a09cb22f438dcc50c9fda97cf7558e
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-01 22:14:30 -0600

    Protoype messaging

--- src/rpc/rpc_dispatcher.rs
+++ src/rpc/rpc_dispatcher.rs
@@ -3,43 +3,68 @@ use crate::rpc::{
     RpcHeader, RpcMessageType, RpcMethodHandler, RpcMethodRegistry, RpcRequest, RpcSessionNode,
     RpcStreamEncoder, RpcStreamEvent,
 };
+use std::cell::RefCell;
+use std::rc::Rc;
 
 pub struct RpcDispatcher<'a> {
-    session: RpcSessionNode<'a>,
+    session: Rc<RefCell<RpcSessionNode<'a>>>,
     next_id: u32, // TODO: Use parent?
     rpc_method_registry: RpcMethodRegistry<'a>,
 }
 
 impl<'a> RpcDispatcher<'a> {
     pub fn new() -> Self {
-        let mut session = RpcSessionNode::new();
+        let session = Rc::new(RefCell::new(RpcSessionNode::new()));
 
-        session.set_response_handler(Box::new(|event: RpcStreamEvent| match event {
-            RpcStreamEvent::Header {
-                rpc_header_id,
-                rpc_header,
-            } => {
-                println!("Received Header: {} - {:?}", rpc_header_id, rpc_header);
-            }
-            RpcStreamEvent::PayloadChunk {
-                rpc_header_id,
-                bytes,
-            } => {
-                println!("Received Payload: {} - {:?}", rpc_header_id, bytes);
-            }
-            RpcStreamEvent::End { rpc_header_id } => {
-                println!("Stream End: {}", rpc_header_id);
-            }
-            RpcStreamEvent::Error {
-                rpc_header_id,
-                frame_decode_error,
-            } => {
-                println!(
-                    "Error in stream {:?}: {:?}",
-                    rpc_header_id, frame_decode_error
-                );
-            }
-        }));
+        // Use a clone of the Rc to move it into the closure, allowing mutable access
+        let session_ref = Rc::clone(&session);
+
+        session
+            .borrow_mut()
+            .set_response_handler(Box::new(move |event: RpcStreamEvent| {
+                // Handle the event here
+                match event {
+                    RpcStreamEvent::Header {
+                        rpc_header_id,
+                        rpc_header,
+                    } => {
+                        println!("Received Header: {} - {:?}", rpc_header_id, rpc_header);
+                    }
+                    RpcStreamEvent::PayloadChunk {
+                        rpc_header_id,
+                        bytes,
+                    } => {
+                        println!("Received Payload: {} - {:?}", rpc_header_id, bytes);
+                    }
+                    RpcStreamEvent::End { rpc_header_id } => {
+                        println!("Stream End: {}", rpc_header_id);
+
+                        // Borrow mutably and use session to start reply stream
+                        session_ref.borrow_mut().start_reply_stream(
+                            RpcHeader {
+                                msg_type: RpcMessageType::Response,
+                                id: rpc_header_id,
+                                method_id: 0,
+                                metadata_bytes: vec![],
+                            },
+                            4,
+                            |bytes: &[u8]| {
+                                // Handle reply bytes here
+                                println!("Reply bytes: {:?}", bytes);
+                            },
+                        );
+                    }
+                    RpcStreamEvent::Error {
+                        rpc_header_id,
+                        frame_decode_error,
+                    } => {
+                        println!(
+                            "Error in stream {:?}: {:?}",
+                            rpc_header_id, frame_decode_error
+                        );
+                    }
+                }
+            }));
 
         Self {
             session,
@@ -103,9 +128,12 @@ impl<'a> RpcDispatcher<'a> {
         };
 
         // Directly pass the closure as `on_emit` without borrowing it
-        let mut encoder =
-            self.session
-                .init_request(hdr, max_chunk_size, on_emit, Some(on_response))?;
+        let mut encoder = self.session.borrow_mut().init_request(
+            hdr,
+            max_chunk_size,
+            on_emit,
+            Some(on_response),
+        )?;
 
         encoder.push_bytes(b"testing 1 2 3")?;
 
@@ -116,6 +144,6 @@ impl<'a> RpcDispatcher<'a> {
     }
 
     pub fn receive_bytes(&mut self, bytes: &[u8]) -> Result<(), FrameDecodeError> {
-        self.session.receive_bytes(bytes)
+        self.session.borrow_mut().receive_bytes(bytes)
     }
 }
