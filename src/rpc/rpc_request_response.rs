09bbdd05385e9d0416953e3daf98fec8d0837628
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-02 12:56:58 -0600

    Prototype

--- src/rpc/rpc_request_response.rs
+++ src/rpc/rpc_request_response.rs
@@ -1,3 +1,5 @@
+// use xxhash_rust::xxh3::xxh3_64;
+
 #[derive(PartialEq, Debug)]
 pub struct RpcRequest {
     pub method_id: u64,
@@ -5,6 +7,13 @@ pub struct RpcRequest {
     pub pre_buffered_payload_bytes: Option<Vec<u8>>,
     pub is_finalized: bool,
 }
+
+// TODO: Re-add?
+// impl RpcRequest {
+//     pub fn to_method_id(method_name: &str) -> u64 {
+//         xxh3_64(method_name.as_bytes())
+//     }
+// }
 
 #[derive(PartialEq, Debug)]
 pub struct RpcResponse {
