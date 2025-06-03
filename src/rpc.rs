262accacd723362e4b14478ec0a7ed964ec006e1
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-03 13:54:04 -0600

    Add `RpcResultStatus` enum

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,6 +1,8 @@
 mod rpc_dispatcher;
 pub mod rpc_internals;
 mod rpc_request_response;
+mod rpc_result_status;
 
 pub use rpc_dispatcher::RpcDispatcher;
 pub use rpc_request_response::{RpcRequest, RpcResponse};
+pub use rpc_result_status::RpcResultStatus;
---
+++ src/rpc/rpc_result_status.rs
@@ -0,0 +1,28 @@
+#[repr(u8)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum RpcResultStatus {
+    Success = 0,
+    Fail = 1,
+    SystemError = 2,
+}
+
+impl RpcResultStatus {
+    #[inline]
+    pub fn value(self) -> u8 {
+        self as u8
+    }
+}
+
+impl TryFrom<u8> for RpcResultStatus {
+    type Error = ();
+
+    #[inline]
+    fn try_from(v: u8) -> Result<Self, Self::Error> {
+        match v {
+            x if x == RpcResultStatus::Success as u8 => Ok(RpcResultStatus::Success),
+            x if x == RpcResultStatus::Fail as u8 => Ok(RpcResultStatus::Fail),
+            x if x == RpcResultStatus::SystemError as u8 => Ok(RpcResultStatus::SystemError),
+            _ => Err(()),
+        }
+    }
+}
