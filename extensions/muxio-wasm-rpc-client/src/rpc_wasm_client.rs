cc7a7d8e23d456cfe680e0f2d4dbe0189c321b62
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-09 13:59:31 -0600

    It builds using JS callbacks

--- extensions/muxio-wasm-rpc-client/src/rpc_wasm_client.rs
+++ extensions/muxio-wasm-rpc-client/src/rpc_wasm_client.rs
@@ -1,78 +1,38 @@
 use crate::socket_transport::muxio_emit_socket_frame_bytes;
-use futures::channel::mpsc::{UnboundedReceiver, UnboundedSender, unbounded as unbounded_channel};
 use futures::channel::oneshot;
-use futures::stream::StreamExt;
 use js_sys::Uint8Array;
 use muxio::rpc::{
     RpcDispatcher, RpcRequest,
     rpc_internals::{RpcStreamEncoder, RpcStreamEvent},
 };
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::sync::Arc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 use web_sys::console;
 
-thread_local! {
-    static MUXIO_CLIENT_TX_REF: RefCell<Option<UnboundedSender<Vec<u8>>>> =
-        RefCell::new(None);
-    static MUXIO_CLIENT_RX_REF: RefCell<Option<Rc<RefCell<UnboundedReceiver<Vec<u8>>>>>> =
-        RefCell::new(None);
-}
-
-#[wasm_bindgen]
-pub fn muxio_receive_socket_frame_uint8(inbound_data: Uint8Array) -> Result<(), JsValue> {
-    let inbound_bytes = inbound_data.to_vec();
-
-    MUXIO_CLIENT_TX_REF.with(|cell| {
-        if let Some(tx) = cell.borrow().as_ref() {
-            let _ = tx.unbounded_send(inbound_bytes);
-            web_sys::console::log_1(&"emit...".into());
-        } else {
-            console::error_1(&"Client TX not initialized".into());
-        }
-    });
-
-    Ok(())
-}
-
-fn start_recv_loop(rx: Rc<RefCell<UnboundedReceiver<Vec<u8>>>>) {
-    spawn_local(async move {
-        loop {
-            let maybe_bytes = rx.borrow_mut().next().await;
-            if let Some(bytes) = maybe_bytes {
-                web_sys::console::log_1(&"receive...".into());
-                muxio_emit_socket_frame_bytes(bytes.as_slice());
-            } else {
-                break;
-            }
-        }
-    });
-}
-
 pub struct RpcWasmClient {
     pub dispatcher: Arc<std::sync::Mutex<RpcDispatcher<'static>>>,
-    pub tx: UnboundedSender<Vec<u8>>,
+    pub emit_callback: Arc<dyn Fn(Vec<u8>) + Send + Sync>,
+    pub receive_callback: Arc<dyn Fn(Vec<u8>) + Send + Sync>,
 }
 
 impl RpcWasmClient {
-    pub async fn new() -> RpcWasmClient {
+    pub fn new(
+        emit_callback: impl Fn(Vec<u8>) + Send + Sync + 'static,
+        receive_callback: impl Fn(Vec<u8>) + Send + Sync + 'static,
+    ) -> RpcWasmClient {
         let dispatcher = Arc::new(std::sync::Mutex::new(RpcDispatcher::new()));
-        let (tx, rx) = unbounded_channel::<Vec<u8>>();
-        let rx_rc = Rc::new(RefCell::new(rx));
 
-        MUXIO_CLIENT_TX_REF.with(|cell| {
-            *cell.borrow_mut() = Some(tx.clone());
-        });
+        RpcWasmClient {
+            dispatcher,
+            emit_callback: Arc::new(emit_callback),
+            receive_callback: Arc::new(receive_callback),
+        }
+    }
 
-        MUXIO_CLIENT_RX_REF.with(|cell| {
-            *cell.borrow_mut() = Some(rx_rc.clone());
-        });
-
-        start_recv_loop(rx_rc);
-
-        Self { dispatcher, tx }
+    pub fn receive_inbound(&self, bytes: Vec<u8>) {
+        web_sys::console::log_1(&"receive...".into());
+        (self.receive_callback)(bytes);
     }
 
     pub async fn call_rpc<T, F>(
@@ -91,13 +51,13 @@ impl RpcWasmClient {
     {
         web_sys::console::log_1(&"Call RPC...".into());
 
-        let tx = self.tx.clone();
+        let emit = self.emit_callback.clone();
         let (done_tx, done_rx) = oneshot::channel::<T>();
         let done_tx = Arc::new(std::sync::Mutex::new(Some(done_tx)));
         let done_tx_clone = done_tx.clone();
 
         let send_fn: Box<dyn for<'a> FnMut(&'a [u8]) + Send + 'static> = Box::new(move |chunk| {
-            let _ = tx.unbounded_send(chunk.to_vec());
+            emit(chunk.to_vec());
             web_sys::console::log_1(&"emit...".into());
         });
 
@@ -136,4 +96,11 @@ impl RpcWasmClient {
 
         (rpc_stream_encoder, result)
     }
+}
+
+#[wasm_bindgen]
+pub fn muxio_receive_socket_frame_uint8(inbound_data: Uint8Array) -> Result<(), JsValue> {
+    console::log_1(&"muxio_receive_socket_frame_uint8 called".into());
+    // JS must call `.receive_inbound(...)` on the actual client instance.
+    Ok(())
 }
