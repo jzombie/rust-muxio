7476fa782c02ff64c7df8ae20882753d04c48e97
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-11 11:45:24 -0600

    Rename to `RpcStreamEventDecoderHandler`

--- src/rpc/rpc_internals/rpc_trait.rs
+++ src/rpc/rpc_internals/rpc_trait.rs
@@ -7,12 +7,8 @@ impl<T: FnMut(&[u8])> RpcEmit for T {}
 pub trait RpcResponseHandler: FnMut(RpcStreamEvent) + Send {}
 impl<T: FnMut(RpcStreamEvent) + Send> RpcResponseHandler for T {}
 
-pub trait RpcStreamEventHandler: FnMut(RpcStreamEvent) {}
-impl<T: FnMut(RpcStreamEvent)> RpcStreamEventHandler for T {}
-
-// TODO: Rename
-pub trait RpcStreamEventFallibleHandler:
+pub trait RpcStreamEventDecoderHandler:
     FnMut(RpcStreamEvent) -> Result<(), FrameDecodeError>
 {
 }
-impl<T: FnMut(RpcStreamEvent) -> Result<(), FrameDecodeError>> RpcStreamEventFallibleHandler for T {}
+impl<T: FnMut(RpcStreamEvent) -> Result<(), FrameDecodeError>> RpcStreamEventDecoderHandler for T {}
