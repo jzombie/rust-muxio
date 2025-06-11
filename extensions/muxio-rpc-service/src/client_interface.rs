90d2b2c5e3dd714a63795117d9ea9163f810833d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-11 13:08:24 -0600

    Add comment

--- extensions/muxio-rpc-service/src/client_interface.rs
+++ extensions/muxio-rpc-service/src/client_interface.rs
@@ -32,6 +32,6 @@ pub trait RpcClientInterface {
     ) -> Result<(RpcStreamEncoder<Box<dyn RpcEmit + Send + Sync>>, T), io::Error>
     where
         T: Send + 'static,
-        // TODO: Use type alias for `Fn(&[u8])`
+        // TODO: Use type alias for `Fn(&[u8])` (Note: This is a wrapped variant of `RpcResponseHandler` and not directly compatible)
         F: Fn(&[u8]) -> T + Send + Sync + 'static;
 }
