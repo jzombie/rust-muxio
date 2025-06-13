3704e4daa7bc402e88413edf7d3967d6fb2c9d3e
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-13 11:11:12 -0600

    Add TODOs

--- src/rpc/rpc_internals/rpc_stream_event.rs
+++ src/rpc/rpc_internals/rpc_stream_event.rs
@@ -9,15 +9,18 @@ pub enum RpcStreamEvent {
         rpc_header: RpcHeader,
     },
     PayloadChunk {
+        // TODO: For ease of use, add `rpc_header` here?
         rpc_request_id: u32,
         rpc_method_id: u64,
         bytes: Vec<u8>,
     },
     End {
+        // TODO: For ease of use, add `rpc_header` here?
         rpc_request_id: u32,
         rpc_method_id: u64,
     },
     Error {
+        // TODO: For ease of use, add `Option<RpcHeader>` here?
         rpc_request_id: Option<u32>,
         rpc_method_id: Option<u64>,
         frame_decode_error: FrameDecodeError,
